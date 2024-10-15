098fabe673d40a7421c82092b537c0ce3d51d1e1
Scherazade Odoi <scherazadeodoi366@gmail.com>
2024-10-15 15:31:20 +0000

    started on the paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Main from './pages/Main';
+import UserDashboardLayout from './layouts/UserDashboardLayout';
+import VendorDashboardLayout from './layouts/VendorDashboardLayout';
+
+
+
+
 
 function App() {
-  
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <Main/>,
+    },
+    {
+      path:"/userdash",
+      element: <UserDashboardLayout/>
+    },
+    {
+      path:"/vendordash",
+      element: <VendorDashboardLayout/>
+    }
 
-  return (
-    <>
-      
-    </>
-  )
+  ]);
+
+  
+  return <RouterProvider router ={router}/>;
 }
 
 export default App
