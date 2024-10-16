6403432d79afd30d9e84b0c49355a98787fbb23b
Henrrike Quarshie <henrrikequarshie@gmail.com>
2024-10-16 15:41:33 +0000

    worked on main

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='img bg-black-500 opacity-75'>
+      <section>
+      <div>
+        <span>Icon</span>
+        <span>Sleek</span>
+      </div>
+      <h1>Welcome to Sleek</h1>
+      <p>Where do you want to go?</p>
+      <div>
+        <span>SignUp as a Vendor</span>
+        <span>logo</span>
+      </div>
+      <div>
+        <span>SignUp as a User</span>
+        <span>logo</span>
+      </div>
+      </section>
+      
+
+
+    </div>
   )
 }
 
--- src/pages/registerForm/VendorRegisterForm.jsx
+++ src/pages/registerForm/VendorRegisterForm.jsx
@@ -1,47 +1,51 @@
 import React from 'react'
 
 const VendorRegisterForm = () => {
-  return (
-    <div className='flex flex-col justify-center'>
-        <h1 className='font-bold text-[2.5rem] text-center'>Register</h1>
-
-<form className='flex flex-col  border border-green-600  gap-y-3 h-[90vh] w-[40vw] self-center'>
+    return (
+        <div className='flex flex-col justify-center h-[100vh] bg-[#F8F8F8]'>
 
 
-  <div className='gap-y-2'>
-    <label >Full Name</label>
-    <input type="text" />
-  </div>
+            <form className='flex flex-col  border gap-y-3 h-[95vh] w-[40vw] self-center pl-8 bg-[#FFFFFF]'>
 
-  <div>
-    <label>Username</label>
-    <input type="text" />
-  </div>
+                <h1 className=' text-[2rem] mt-8'>Register</h1>
+
+                <div className=' flex flex-col gap-y-5 items-end pr-7'>
+                <div className='pl-3 mt-8'>
+                    <label className='font-bold'>Full Name</label>
+                    <input type="text" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]' />
+                </div>
+
+                <div className='' >
+                    <label className='font-bold'>Username</label>
+                    <input type="text" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]'/>
+                </div>
 
 
-  <div>
-    <label>Email</label>
-    <input type="email" name="email" />
-  </div>
+                <div className=''>
+                    <label className='font-bold'>Email</label>
+                    <input type="email" name="email" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]' />
+                </div>
 
-  <div>
-    <label>Password</label>
-    <input type="password" name="password" />
-  </div>
+                <div className=''>
+                    <label className='font-bold'>Password</label>
+                    <input type="password" name="password" className='border-2 ml-4 p-2 px-20 pl-2 w-80 bg-[#F8F8F8]'/>
+                </div>
 
-  <div>
-    <label>Confirm password</label>
-    <input type="password" name="" />
-  </div>
-  <div>
-    <label>Phone</label>
-    <input type="phone" name="" />
-  </div>
-
-  <button>Register</button>
-</form>
-    </div>
-  )
+                <div className=''>
+                    <label className='font-bold'>Confirm password</label>
+                    <input type="password" name="" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]'/>
+                </div>
+                <div>
+                    <label className='font-bold'>Phone</label>
+                    <input type="phone" name="" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]' />
+                </div>
+                </div>
+                 <div>
+                 <button className='border-2 p-4 py-2 ml-40 mt-4 h-10 w-28 bg-green-600 text-center text-white border-green-600'>Register</button>
+                 </div>
+            </form>
+        </div>
+    )
 }
 
 export default VendorRegisterForm;
