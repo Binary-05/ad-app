d4ba771174f6404899ae4559764e779e9d02c7a2
Scherazade Odoi <scherazadeodoi366@gmail.com>
2024-10-16 12:57:32 +0000

    form

--- src/pages/registerForm/RegisterForm.jsx
+++ src/pages/registerForm/RegisterForm.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const RegisterForm = () => {
   return (
-    <div className=' h-full w-full items-center'>
+    <div className=' flex flex-col justify-center'>
 
-      <h1 className='font-bold text-[2.5rem]'>Register</h1>
+      <h1 className='font-bold text-[2.5rem] text-center'>Register</h1>
 
-      <form className='flex flex-col  border border-green-600  gap-y-3 h-[90vh] w-[40vw] self-center '>
+      <form className='flex flex-col  border border-green-600  gap-y-3 h-[90vh] w-[40vw] '>
 
 
         <div className='gap-y-2'>
           <label >Full Name</label>
-          <input type="text" />
+          <input type="text" className='' />
         </div>
 
         <div>
