9cf5655f489482cc48bb2a7965a4ca1340c065ee
Henrrike Quarshie <henrrikequarshie@gmail.com>
2024-10-21 14:05:23 +0000

    working on endpoints

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import { FaAngleRight } from "react-icons/fa";
 
 const Main = () => {
   return (
-    <div className='img bg-black-500 opacity-75'>
-      <section className='flex flex-col p-44 gap-y-7'>
-      <div>
-        <span>Icon</span>
-        <span className='text-[25px] font-bold italic'>Sleek</span>
-      </div>
-      <h1 className='text-[3rem] font-bold'>Welcome to Sleek</h1>
-      <p className='font-semibold text-[25px]'>Where do you want to go?</p>
-      <div>
-        <span>SignUp as a Vendor</span>
-        <span>logo</span>
-      </div>
-      <div>
-        <span>SignUp as a User</span>
-        <span>logo</span>
-      </div>
+    <div className='img'>
+      <section className='flex flex-col gap-y-7 text-white'>
+        <div>
+          <span>Icon</span>
+          <span className='text-[30px] font-bold italic'>Sleek</span>
+        </div>
+        <h1 className='flex justify-center text-[5rem] font-bold'>Welcome to Sleek</h1>
+        <p className='flex justify-center font-semibold text-[30px]'>Where would you like to go?</p>
+        <div className='flex justify-between ml-80 mr-80 text-[25px] font-bold'>
+        <div className='flex border-b-2 pb-2 border-green-700'>
+          <span>SignUp as a Vendor</span>
+          <span className="pt-2.5 hover:text-green-700"><FaAngleRight /></span>
+        </div>
+        <div className='flex border-b-2 pb-2 border-green-700'>
+          <span>SignUp as a User</span>
+          <span className="pt-2.5 hover:text-green-700"><FaAngleRight /></span>
+        </div>
+        </div>
       </section>
-      
+
 
 
     </div>
--- src/pages/registerForm/VendorRegisterForm.jsx
+++ src/pages/registerForm/VendorRegisterForm.jsx
@@ -1,50 +1,56 @@
 import React from 'react'
+// import { reg } from "./src/App.css" 
 
 const VendorRegisterForm = () => {
     return (
-        <div className='flex flex-col justify-center h-[100vh] bg-[#F8F8F8]'>
+           
+            <div className='reg flex flex-col justify-center h-[100vh] bg-[#F8F8F8]'>
 
 
-            <form className='flex flex-col  border gap-y-3 h-[95vh] w-[40vw] self-center pl-8 bg-[#FFFFFF]'>
+                <form className='flex flex-col  border gap-y-3 h-[130vh] w-[40vw] self-center pl-8 bg-[#FFFFFF]'>
 
-                <h1 className=' text-[2rem] mt-8'>Register</h1>
+                    <h1 className=' text-[2rem] mt-8'>Register</h1>
 
-                <div className=' flex flex-col gap-y-5 items-end pr-7'>
-                <div className='pl-3 mt-8'>
-                    <label className='font-bold'>Full Name</label>
-                    <input type="text" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]' />
-                </div>
+                    <div className=' flex flex-col gap-y-5 items-end pr-7'>
+                        <div className='pl-3'>
+                            <label className='font-bold'>First Name</label>
+                            <input type="text" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]' />
+                        </div>
+                        <div className='pl-3 mt-1'>
+                            <label className='font-bold'>Last Name</label>
+                            <input type="text" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]' />
+                        </div>
 
-                <div className='' >
-                    <label className='font-bold'>Username</label>
-                    <input type="text" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]'/>
-                </div>
+                        <div className='' >
+                            <label className='font-bold'>Username</label>
+                            <input type="text" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]' />
+                        </div>
 
 
-                <div className=''>
-                    <label className='font-bold'>Email</label>
-                    <input type="email" name="email" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]' />
-                </div>
+                        <div className=''>
+                            <label className='font-bold'>Email</label>
+                            <input type="email" name="email" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]' />
+                        </div>
 
-                <div className=''>
-                    <label className='font-bold'>Password</label>
-                    <input type="password" name="password" className='border-2 ml-4 p-2 px-20 pl-2 w-80 bg-[#F8F8F8]'/>
-                </div>
+                        <div className=''>
+                            <label className='font-bold'>Password</label>
+                            <input type="password" name="password" className='border-2 ml-4 p-2 px-20 pl-2 w-80 bg-[#F8F8F8]' />
+                        </div>
 
-                <div className=''>
-                    <label className='font-bold'>Confirm password</label>
-                    <input type="password" name="" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]'/>
-                </div>
-                <div>
-                    <label className='font-bold'>Phone</label>
-                    <input type="phone" name="" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]' />
-                </div>
-                </div>
-                 <div>
-                 <button className='border-2 p-4 py-2 ml-40 mt-4 h-10 w-28 bg-green-600 text-center text-white border-green-600'>Register</button>
-                 </div>
-            </form>
-        </div>
+                        <div className=''>
+                            <label className='font-bold'>Confirm password</label>
+                            <input type="password" name="" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]' />
+                        </div>
+                        <div>
+                            <label className='font-bold'>Phone</label>
+                            <input type="phone" name="" className='border-2 ml-4 p-2 px-16 w-80 bg-[#F8F8F8]' />
+                        </div>
+                    </div>
+                    <div>
+                        <button className='border-2 p-4 py-2 ml-40 mt-4 h-10 w-28 bg-green-600 text-center text-white border-green-600'>Register</button>
+                    </div>
+                </form>
+            </div>
     )
 }
 
