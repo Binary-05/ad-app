c74c82279a1a0cdf9bb7dfc04d442b4c981f8687
Scherazade Odoi <scherazadeodoi366@gmail.com>
2024-10-21 09:30:06 +0000

    worked on the mapping

---
+++ src/pages/UserPage/constant/index.js
@@ -0,0 +1,15 @@
+
+
+const K ={
+  ADLISTS:[
+    {
+        media: "hi",
+        title: "hello",
+        price: 2,
+        description: "yo",
+        category: "whatsup"
+    }
+  ]
+};
+
+export default K;
--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+// import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
+import K from './constant';
+
+// const adlists = () =>{
+//   const [ads, setAds]= useState([]);
+//   const getAds = async () => {
+// const response = await axios.get (`${BASE_URL}/ads`);
+// setAds(response.data);
+//   }
+
+
+// useEffect(() => {
+//   getAds();
+// }, []);
+
+
+// };
+
 
 const UserPage = () => {
   return (
-    <div>UserPage
-      <Navbar/>
-
-      <Footer/>
+    <div>
+      <Navbar />
+      <div>
+        {
+          K.ADLISTS.map((ad, index) => {
+            console.log (ad);
+            return (
+              <div key = {index}>
+                {/* <p>{ad.media}</p>
+                <p>{ad.price}</p>
+                <p>{ad.title}</p>
+                <p>{ad.description}</p>
+                <p>{ad.category}</p> */}
+              </div>
+            )
+          })
+        }
+      </div>
+      <Footer />
     </div>
   )
-}
+};
 
 export default UserPage;
