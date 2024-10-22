1e1157d045bf6d12e340602ba307d3732e518037
Scherazade Odoi <scherazadeodoi366@gmail.com>
2024-10-22 15:57:47 +0000

    changed the name on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex justify-evenly h-16 bg-black place-items-center fixed left-0 right-0">
-      <h1 className="text-3xl text-white">Sleek</h1>
+      <h1 className="text-3xl text-white">B-ADs</h1>
       <input type="search" name="search" id="" className="h-12 w-6/12 rounded-md" />
       <button className="bg-green-600 text-white py-3 px-9 rounded-lg">Search</button>
 
--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -3,7 +3,7 @@ import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import K from "./constant";
 import { apiGetAdvert } from "../../services/advert";
-import axios from "axios";
+
 
 
 
@@ -22,18 +22,19 @@ const UserPage = () => {
   return (
     <div>
       <Navbar />
-      <div className="">
-        <div className="pt-16">
-          <p>FEATURED PRODUCTS</p>
-          <div>
+      <div>
+        <div className="pt-28 ">
+          <p className="text-4xl py-3 ">ADVERTISMENTS</p>
+          <div className=" grid grid-cols-3 p-10 border shadow-md h-96 rounded-sm mx-10">
             {
               ads.map((ad, index) => {
-                console.log(`${index}: ${ad.title}`);
+                console.log(`${index}: ${ad.media}`);
                 return <div key={index}>
-                  <div>
+                  <div className="border shadow-sm">
                     <img src={ad.media} alt="" />
                   </div>
                   <p>{ad.title}</p>
+                  <p>{ad.price}</p>
                   <p>{ad.description}</p>
                 </div>
               })
