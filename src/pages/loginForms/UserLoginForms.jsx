61114c9d6503c028407d286b57d89f10a1ec3e39
Scherazade Odoi <scherazadeodoi366@gmail.com>
2024-10-23 13:17:42 +0000

    worked on the login form

--- src/pages/loginForms/UserLoginForms.jsx
+++ src/pages/loginForms/UserLoginForms.jsx
@@ -1,29 +1,45 @@
 import { MdCheckBoxOutlineBlank } from "react-icons/md";
+import { apiSignin } from "../../services/auth";
 
 const UserLoginForms = () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    //logic to handle login goes here 
+    const formData = new FormData(event.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    console.log("email", email, "password", password);
+    //console.log  ('email', email,)
+    const response = await apiSignin({ email, password });
+    // console.log(response.data)
+    if (response.status === 200) {
+      localStorage.setItem("token",response.data.accessToken)
+    }
+  };
+
   return (
-    <div>
-<div>
+    <div className="log">
+      <div>
         <p className="flex text-[25px] font-semibold pl-96 pr-16 mt-8">Login as a User</p>
-        <form className=' border-2 w-[35vw] h-[80vh] ml-96 mt-5'>
+        <form onSubmit={handleSubmit} className=' border-2 w-[35vw] h-[80vh] ml-96 mt-5 bg-white '>
           <p className='flex text-[28px] justify-center pb-5'>Login</p>
           <div className="pl-16">
-          <div className='flex flex-col'>
-            <label htmlFor="">Email</label>
-            <input type="text" className='border-2 w-[25vw] p-2 rounded-lg mb-3' />
-          </div>
-          <div className='flex flex-col'>
-            <label htmlFor="">Password</label>
-            <input type="text" className='border-2 w-[25vw] p-2 rounded-lg mb-3'/>
-          </div>
-          <div className="flex mb-3">
-            <div className='flex'>
-              <p className="pt-1 rounded-lg"><MdCheckBoxOutlineBlank /></p>
-              <p>Show Password</p>
+            <div className='flex flex-col'>
+              <label htmlFor="">Email</label>
+              <input type="text" name="email" className='border-2 w-[25vw] p-2 rounded-lg mb-3' />
             </div>
-            <p className="pl-24 text-blue-700">Forgot Password?</p>
-          </div>
-          <button className="boredr-2 bg-blue-700 text-white p-2 w-[25vw] rounded-lg mb-2">LOGIN</button>
+            <div className='flex flex-col'>
+              <label htmlFor="">Password</label>
+              <input type="password" name="password" className='border-2 w-[25vw] p-2 rounded-lg mb-3' />
+            </div>
+            <div className="flex mb-3">
+              <div className='flex'>
+                <p className="pt-1 rounded-lg"><MdCheckBoxOutlineBlank /></p>
+                <p>Show Password</p>
+              </div>
+              <p className="pl-24 text-blue-700">Forgot Password?</p>
+            </div>
+            <button type="submit" className="boredr-2 bg-blue-700 text-white p-2 w-[25vw] rounded-lg mb-2">LOGIN</button>
           </div>
           <div className="flex justify-center">
             <p>Don't have an account?</p>
--- src/pages/registerForm/RegisterForm.jsx
+++ src/pages/registerForm/RegisterForm.jsx
@@ -1,23 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { apiSignup } from '../../services/auth'
 
 const RegisterForm = () => {
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate()
+  const handleSubmit = async (event) => {
+    event.preventDefault() //prevents page from reloading
+
+    try {
+      //prepare data for the backend
+      setLoading(true)
+      const formData = new FormData(event.target) //grabs data from the form
+
+      const businessName = formData.get("businessName")
+      const firstName = formData.get("firstName")
+      const lastName = formData.get("lastName")
+      const email = formData.get("email")
+      const password = formData.get("password")
+      // const role = formData.get("role")
+      console.log('first name', firstName)
+
+      //Check if passwords match
+      // if(password1 !== password2){
+      //    return
+
+      // }
+
+      // if key and value are same, pick one eg. firstName, if not then state both separated with a colon (:) eg. firstName: firstName,
+      const payload = { businessName, firstName, lastName, email, password, role: "vendor" }
+
+      const response = await apiSignup(payload)
+      console.log(response.data)
+
+      //Show a success notification
+
+      navigate("/login") //takes user to the login page after successful registration
+
+
+    } catch (error) {
+console.log(error)
+    } finally{
+      setLoading(false)
+    }
+  }
+
   return (
-    <div className=' flex flex-col justify-center pb-20  bg-[#F8F8F8]'>
+    <div className=' flex flex-col justify-center pb-20  bg-[#F8F8F8] '>
 
 
 
-      <form className='flex flex-col border gap-y-3 h-[90vh] w-[40vw] self-center px-5 py-10 justify-evenly mt-20 bg-white'>
+      <form onSubmit={handleSubmit} className='flex flex-col border gap-y-3 h-[90vh] w-[40vw] self-center px-5 py-10 justify-evenly mt-20 bg-white'>
 
         <h1 className=' text-[2.5rem] pb-5'>Register</h1>
 
+        <div>
+          <label className='font-semibold pl-8 '>Business Name</label>
+          <input type="text" name="businessName" className='w-2/3 h-10 ml-4 border-2 bg-[#F8F8F8]' />
+        </div>
+
         <div >
-          <label className='font-semibold pl-16' >Full Name</label>
-          <input type="text" className='w-2/3 h-10 ml-4 border-2 bg-[#F8F8F8]' />
+          <label className='font-semibold pl-16' >First Name</label>
+          <input type="text" name='firstName' className='w-2/3 h-10 ml-4 border-2 bg-[#F8F8F8]' />
         </div>
 
         <div>
-          <label className='font-semibold pl-16'>Username</label>
-          <input type="text" className='w-2/3 h-10 ml-4 border-2 bg-[#F8F8F8]' />
+          <label className='font-semibold pl-16'>Last Name</label>
+          <input type="text" name='lastName' className='w-2/3 h-10 ml-4 border-2 bg-[#F8F8F8]' />
         </div>
 
 
@@ -31,12 +81,8 @@ const RegisterForm = () => {
           <input type="password" name="password" className='w-2/3 h-10 ml-4 border-2 bg-[#F8F8F8]' />
         </div>
 
-        <div>
-          <label className='font-semibold '>Confirm password</label>
-          <input type="password" name="" className='w-2/3 h-10 ml-4 border-2 bg-[#F8F8F8]' />
-        </div>
 
-        <button className='border border-green-600 bg-green-600 text-white h-10 w-28 mt-4 ml-36'>Register</button>
+        <button className='border border-green-600 bg-green-600 text-white h-10 w-28 mt-4 ml-36'>{loading ? "Loading..." : "Register"}</button>
       </form>
 
     </div>
